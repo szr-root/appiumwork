9eea87faea9ea7b8d3052f0ba6215d30dd420d08
Administrator <405260188@qq.com>
2021-02-20 16:20:41 +0800

    第二次作业

--- appium_po/page/base_page.py
+++
@@ -1,9 +0,0 @@
-from appium.webdriver.webdriver import WebDriver
-
-
-class BasePage:
-    def __init__(self, driver: WebDriver = None):
-        self.driver = driver
-
-    def find(self, locator):
-        return self.driver.find_element(*locator)
